758a0309c9bf74706d9a03cec106968e3b30ec8c
yakuznecov <yakuznecov@mail.ru>
2022-05-20 11:44:39 +0300

    fix mask

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 (function ($) {
 	$(document).ready(function ($) {
 		if ($('#payKeeperCard').length > 0) {
-			$('#payKeeperCard').mask('9999 9999 9999 9999 999');
+			$('#payKeeperCard').mask('9999  9999  9999  9999  999', { placeholder: '0' });
 		}
 	});
 })(jQuery);
